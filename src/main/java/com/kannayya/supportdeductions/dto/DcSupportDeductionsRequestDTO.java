0a5c7b6ac624b07c1abde920b23fd825591a1e9a
Vriddhi Darak <vriddhi_darak@epam.com>
2024-09-08 18:07:20 +0530

    using Dtos

---
+++ src/main/java/com/kannayya/supportdeductions/dto/DcSupportDeductionsRequestDTO.java
@@ -0,0 +1,57 @@
+package com.kannayya.supportdeductions.dto;
+
+import com.kannayya.supportdeductions.entity.DcSupportDeductions;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DcSupportDeductionsRequestDTO {
+
+    private Long indvId;
+
+    private String name;
+
+    private Date effBeginDt;
+
+    private Date effEndDt;
+
+    private Date reportedDt;
+
+    private Date clientAwareDt;
+
+    private String expenseTypeCd;
+
+    private BigDecimal monthlyActualAmt;
+
+    private String expAmtVerf;
+
+    private Date finalExpenseDt;
+
+    private Date createDt;
+
+    private Date updateDt;
+
+  public DcSupportDeductions toEntity() {
+        DcSupportDeductionsRequestDTO dto = this;
+        DcSupportDeductions entity = new DcSupportDeductions();
+        entity.setIndvId(dto.getIndvId());
+        entity.setName(dto.getName());
+        entity.setEffBeginDt(dto.getEffBeginDt());
+        entity.setEffEndDt(dto.getEffEndDt());
+        entity.setReportedDt(dto.getReportedDt());
+        entity.setClientAwareDt(dto.getClientAwareDt());
+        entity.setExpenseTypeCd(dto.getExpenseTypeCd());
+        entity.setMonthlyActualAmt(dto.getMonthlyActualAmt());
+        entity.setExpAmtVerf(dto.getExpAmtVerf());
+        entity.setFinalExpenseDt(dto.getFinalExpenseDt());
+        entity.setCreateDt(dto.getCreateDt());
+        entity.setUpdateDt(dto.getUpdateDt());
+        return entity;
+    }
+}
---
+++ src/main/java/com/kannayya/supportdeductions/dto/DcSupportDeductionsResponseDTO.java
@@ -0,0 +1,65 @@
+package com.kannayya.supportdeductions.dto;
+import com.kannayya.supportdeductions.entity.DcSupportDeductions;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DcSupportDeductionsResponseDTO {
+    private LocalDateTime requestTimeStamp;
+
+    private LocalDateTime responseTimeStamp;
+
+    private Long sprtSeqNum;
+
+    private Long indvId;
+
+    private String name;
+
+    private Date effBeginDt;
+
+    private Date effEndDt;
+
+    private Date reportedDt;
+
+    private Date clientAwareDt;
+
+    private String expenseTypeCd;
+
+    private BigDecimal monthlyActualAmt;
+
+    private String expAmtVerf;
+
+    private Date finalExpenseDt;
+
+    private Date createDt;
+
+    private Date updateDt;
+
+    public static DcSupportDeductionsResponseDTO fromEntity(DcSupportDeductions entity, LocalDateTime requestTimeStamp, LocalDateTime responseTimeStamp) {
+        return new DcSupportDeductionsResponseDTO(
+            requestTimeStamp,
+            responseTimeStamp,
+            entity.getSprtSeqNum(),
+            entity.getIndvId(),
+            entity.getName(),
+            entity.getEffBeginDt(),
+            entity.getEffEndDt(),
+            entity.getReportedDt(),
+            entity.getClientAwareDt(),
+            entity.getExpenseTypeCd(),
+            entity.getMonthlyActualAmt(),
+            entity.getExpAmtVerf(),
+            entity.getFinalExpenseDt(),
+            entity.getCreateDt(),
+            entity.getUpdateDt()
+        );
+    }
+
+}
